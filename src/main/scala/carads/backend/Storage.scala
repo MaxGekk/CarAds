9c1b611d77be087f3d76b9943bfd8511c98ff39f
Maxim Gekk <max.gekk@gmail.com>
2017-06-03 14:55:00 +0200

    Moving of the Record class to a separate file

--- src/main/scala/carads/backend/Storage.scala
+++ src/main/scala/carads/backend/Storage.scala
@@ -1,19 +1,7 @@
 package carads.backend
 
-import java.util.Date
-
 import scala.util.Try
 
-sealed trait Fuel
-case class Gasoline() extends Fuel {
-  override def toString: String = "Gasoline"
-}
-case class Diesel() extends Fuel {
-  override def toString: String = "Diesel"
-}
-
-case class Record(id: Int, title: String, fuel: Fuel, price: Int, `new`: Boolean,
-                 mileage: Option[Int], registration: Option[Date])
 trait Storage {
   def createTable: Try[String]
 
--- src/main/scala/carads/frontend/RequestHandler.scala
+++ src/main/scala/carads/frontend/RequestHandler.scala
@@ -3,10 +3,9 @@ package carads.frontend
 import carads.backend.Storage
 import org.json4s.{DefaultFormats, Formats}
 import spray.routing.{HttpServiceActor, RequestContext}
-
 import scala.util.{Failure, Success}
 
-/** The actors receive Star API requests, handle them and send Star API responses or errors. */
+/** The actors receive CarAds API requests, handle them and send CarAds API responses or errors. */
 class RequestHandler(settings: Settings) extends HttpServiceActor with Routes {
   override implicit def json4sJacksonFormats: Formats = DefaultFormats.withBigDecimal
   override val receive = runRoute(routes)
